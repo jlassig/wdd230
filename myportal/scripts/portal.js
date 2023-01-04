4c3f5fe863d8f59ad60d5da30e4ea6d6a33a2524
jlassig <las22004@byui.edu>
2023-01-03 19:35:23 -0700

    added js, updated pic, responsive design on css,

---
+++ myportal/scripts/portal.js
@@ -0,0 +1,23 @@
+//putting the current year for the copyright:
+//first get today's date:
+const y = new Date();
+//get JUST the current year:
+const year = y.getFullYear();
+//now put the current year in the HTML where it says "copy-right-year":
+document.querySelector("#copyright-year").innerHTML = year;
+
+
+//putting the "last updated" at the bottom of the footer:
+//the following is the string we want to write, including the last time the document was modified: 
+let dateModified = ('Last updated: ' + document.lastModified);
+//then we search the document by the ID, in this case "date-mod", and put in the string found in dateModified in that spot: 
+document.getElementById("date-mod").innerHTML = dateModified;
+
+
+
+
+
+
+
+
+
