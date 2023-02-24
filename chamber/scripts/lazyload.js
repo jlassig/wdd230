6ca0af9ce8480f232e968b1b903edaababf5cc62
jlassig <las22004@byui.edu>
2023-02-23 20:12:35 -0700

    fixed lazy loading on my discover page

--- chamber/scripts/lazyload.js
+++ chamber/scripts/lazyload.js
@@ -4,25 +4,31 @@
 //note: querySelector here is looking for ALL img elements with an attribute of data-src. Becaues we wrote the data-src in the brackets, Javascript knows that is an attribute.
 const imagesToLoad =document.querySelectorAll('img[data-src]');
 
+const imgOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px 50px 0px"
+};
 //loadImages moves the path from data-src to src, and then data-src gets removed
 const loadImages = (image) => {
  image.setAttribute('src', image.getAttribute('data-src'));
- image.onload = () =>{image.removeAttribute('data-src');};
+ image.onload = () => {
+  image.removeAttribute('data-src');};
 };
 
 //checking to see if interserctionObserver is supported on the user's screen
 //IntersectionObserver will only load target images when the user scrolls down. 
 if ('IntersectionObserver' in window) {
-  const observer = new IntersectionObserver((items) => {
+  const observer = new IntersectionObserver((items, observer) => {
     items.forEach((item) => {
       if (item.isIntersecting) {
         loadImages(item.target);
         observer.unobserve(item.target);
       }
     });
-  });
+  }, imgOptions);
   //loop through each image and load it
   imagesToLoad.forEach((img) => {
+
     observer.observe(img);
   });
   //load All the images if IntersectionObserver is not supported.
