ec4d1693c5cb648f3a1191ad9eb6849ab34a3679
jlassig <las22004@byui.edu>
2023-03-31 17:35:27 -0600

    added more to bountiful home page

--- bountiful/scripts/weather.js
+++ bountiful/scripts/weather.js
@@ -1,18 +1,5 @@
 // // select HTML elements in the document
-const currentTempSpan = document.querySelector("#temp")
-const weatherIconDiv = document.querySelector("#weather-icon-div")
-const weatherType = document.querySelector("#weather-type")
-const humidityDiv = document.querySelector("#humidity-div")
 
-const currentTempSpan2 = document.querySelector("#temp2")
-const weatherIconDiv2 = document.querySelector("#weather-icon-div2")
-const weatherType2 = document.querySelector("#weather-type2")
-const humidityDiv2 = document.querySelector("#humidity-div2")
-
-const currentTempSpan3 = document.querySelector("#temp3")
-const weatherIconDiv3 = document.querySelector("#weather-icon-div3")
-const weatherType3 = document.querySelector("#weather-type3")
-const humidityDiv3 = document.querySelector("#humidity-div3")
 
 const url =
   "https://api.openweathermap.org/data/2.5/forecast?lat=33.1581&lon=-117.3506&units=imperial&appid=1332197040ea80518ea099b97ec567b3"
@@ -22,7 +9,7 @@ async function apiFetch() {
     const response = await fetch(url)
     if (response.ok) {
       const data = await response.json()
-      console.log(data) // this is for testing the call
+      // console.log(data) // this is for testing the call
       displayResults(data)
     } else {
       throw Error(await response.text())
@@ -33,31 +20,41 @@ async function apiFetch() {
 }
 
 function displayResults(weatherdata) {
-  // let temp = weatherdata.main.temp.toFixed(0)
-  let temp = weatherdata.list[0].main.temp.toFixed(0)
-  let humidityInfo = weatherdata.list[0].main.humidity
+const weatherCards = document.querySelector("#weather-cards")
+  for (let i=0; i < weatherdata.list.length; i += 8)
+  {
+    let card = document.createElement('div')
+    let dateDiv = document.createElement('div')
+    let tempDiv = document.createElement('div')
+    let tempSpan = document.createElement('span')
+    let weatherIconDiv = document.createElement('div')
+    let weatherIcon = document.createElement('img')
+    let humidityDiv = document.createElement('div')
+    let weatherSrc = `https://openweathermap.org/img/w/${weatherdata.list[i].weather[0].icon}.png`
+    let descriptionDiv = document.createElement('div')
+    let description = toTitleCase(weatherdata.list[i].weather[0].description)
+    let date = formatDate(weatherdata.list[i].dt_txt)
+    dateDiv.textContent = date
+    tempDiv.textContent = `Temp: ${weatherdata.list[i].main.temp.toFixed(0)}`
+    tempSpan.innerHTML = "&#xb0;F"
+    humidityDiv.textContent = `Humidity: ${weatherdata.list[i].main.humidity}`
+    descriptionDiv.textContent = description
 
-  let temp2 = weatherdata.list[1].main.temp.toFixed(0)
-  let humidityInfo2 = weatherdata.list[1].main.humidity
+    //appendChild
+    card.appendChild(dateDiv)
+    card.appendChild(tempDiv)
+    tempDiv.appendChild(tempSpan)
+    card.appendChild(humidityDiv)
+    card.appendChild(descriptionDiv)
+    weatherCards.appendChild(card)
+    
 
-  currentTempSpan.innerHTML = `<strong>${temp}</strong>`
-  humidityDiv.innerHTML = `Humidity: ${humidityInfo}%`
+  if(i>=16)
+  {
+    break;
+  }
+  }  
 
-  currentTempSpan2.innerHTML = temp2
-  humidityDiv2.innerHTML = `Humidity: ${humidityInfo}%`
-
-  // let iconsrc = `https://openweathermap.org/img/w/${weatherdata.weather[0].icon}.png`
-  // let desc = weatherdata.weather[0].description
-  // let descriptionTitle = toTitleCase(desc)
-
-  // let weatherIcon = document.createElement("img")
-  // weatherIcon.setAttribute("src", iconsrc)
-  // weatherIcon.setAttribute("alt", descriptionTitle)
-  // weatherIcon.setAttribute("width", "75")
-  // weatherIcon.setAttribute("height", "75")
-  // weatherType.textContent = descriptionTitle
-
-  // weatherIconDiv.appendChild(weatherIcon)
 }
 
 apiFetch()
@@ -69,4 +66,57 @@ function toTitleCase(string) {
   }
   sentence = sentence.join(" ")
   return sentence
+}
+function formatDate(string)
+{
+  let month = ""
+  let year = string.slice(0,4)
+  let monthNum = string.slice(5,7)
+  let day = string.slice(8,10)
+
+  switch (monthNum)
+  {
+    case "01":
+      month = "January"
+      break;
+    case "02":
+      month = "February"
+      break;
+    case "03":
+      month = "March"
+      break;
+    case "04":
+      month = "April"
+      break;
+    case "05":
+      month = "May"
+      break;
+    case "06":
+      month = "June"
+      break;
+    case "07":
+      month = "July"
+      break;
+    case "08":
+      month = "August"
+      break;
+    case "09":
+      month = "September"
+      break;
+    case "10":
+      month = "October"
+      break;
+    case "11":
+      month = "November"
+      break;
+    case "12":
+      month = "December"
+      break;
+    default:
+      month = "month"
+      break;
+  }
+
+  return `${month} ${day}, ${year}`
+  
 }
